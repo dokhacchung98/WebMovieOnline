32693c15963482b05be86230d70c37e38186f0c6
Chung Đỗ <ochung3@gmail.com>
2019-01-29 17:29:34 +0700

    add generic repository pattern to common project

---
+++ Common/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.GenericRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetEntityById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        Task Add(TEntity entity);
+        Task Delete(TEntity entity);
+        Task Update(TEntity entity);
+    }
+}
