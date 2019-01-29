bbd2c1518a84098672bec98a675cdf3cbd5e0987
Chung Đỗ <ochung3@gmail.com>
2019-01-29 15:30:12 +0700

    Add entity framework to project

---
+++ Infrastructure/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+            IsDeleted = false;
+        }
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public StatusFilmEnum Status { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Infrastructure/Enums/StatusUserEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Enum
+{
+    public enum StatusUserEnum
+    {
+        Online = 1,
+        Offline = 2,
+        Block = 3
+    }
+}
