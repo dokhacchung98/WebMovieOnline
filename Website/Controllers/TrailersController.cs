ad0e60162e3295b0eb25e6fafb6211c90f251691
tiennampham23 <phamtiennam2301@gmail.com>
2019-05-01 13:18:09 +0700

    Fix bugs display trailer

---
+++ Website/Controllers/TrailersController.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationCore.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Website.ViewModel;
+
+namespace Website.Controllers
+{
+    public class TrailersController : Controller
+    {
+        private readonly ITrailerService _trailerService;
+
+        public TrailersController()
+        {
+        }
+
+        public TrailersController(
+            ITrailerService trailerService)
+        {
+            _trailerService = trailerService;
+        }
+
+        public ActionResult NewestTrailers()
+        {
+            var trailers = _trailerService.GetNewTrailerByNumber(10);
+
+            var trailerViewModels = AutoMapper.Mapper.Map<IEnumerable<TrailerViewModel>>(trailers);
+            for (int i = 0; i < trailers.Count(); i++)
+            {
+                var movie = trailers.ToList()[i].Movie;
+                var movieViewModel = AutoMapper.Mapper.Map<MoviesViewModel>(movie);
+                trailerViewModels.ToList()[i].MoviesViewModel = movieViewModel;
+            }
+
+            return PartialView("_NewestTrailers", trailerViewModels);
+        }
+    }
+}
